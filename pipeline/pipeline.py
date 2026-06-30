aa5e00d3f835d978451accf96ceb787aba748095
Matheus Gentil <48365830+mvgentil@users.noreply.github.com>
2026-06-30 19:21:35 +0000

    commit files

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,3 +5,5 @@ df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+print(f"Pipeline completed successfully for day {sys.argv[1]}.")
